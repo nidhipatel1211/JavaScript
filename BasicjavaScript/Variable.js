e2780ff901e17f38ea7ad2c5f896408cd20d2a49
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2024-10-02 20:31:29 +0000

    Variable

---
+++ BasicjavaScript/Variable.js
@@ -0,0 +1,7 @@
+const accountId = 144544
+let accountEmail ="nidhi121125@gmail.com"
+var accountPassword ="12345"
+accountCity = "Vadodara"
+let accountstate
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
