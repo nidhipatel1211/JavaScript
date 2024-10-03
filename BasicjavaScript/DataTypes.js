79491fb2e1960c882dcaed3a82ae89695e193165
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2024-10-03 17:25:23 +0000

    DataType

---
+++ BasicjavaScript/DataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //trate all js code as new version
+
+//alert("Hello") we are using nodejs not browser
+//alert(3+3)
+
+console.log(3+3); 
+console.log("nidhi")
+let name= "nidhi"
+let age= 25
+let isLoggedIn = false
+let state = null
+//symbol = unique
+//object
+console.log(typeof null)
+console.log(typeof undefined)
