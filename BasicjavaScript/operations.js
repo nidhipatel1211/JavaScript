2fab7f115cd40085b5ee4f1f2540de7ed3108613
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2024-10-03 17:43:06 +0000

    Operations

---
+++ BasicjavaScript/operations.js
@@ -0,0 +1,21 @@
+let value=3
+let negval= -value
+console.log(negval)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1= "hello"
+let str2= " Nidhi"
+let str3 = str1+ str2
+console.log(str3)
+console.log("1" +2)
+console.log(1 +2+"2")
+console.log("1" +2+2)
+console.log("1"+"2")
+console.log(+true)
+
+
