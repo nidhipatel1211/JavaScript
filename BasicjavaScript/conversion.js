24975322e21a60572cbfc935e1897e46a0955f78
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2024-10-03 17:33:53 +0000

    Conversion

---
+++ BasicjavaScript/conversion.js
@@ -0,0 +1,12 @@
+let score ="33AB"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valInNum = Number(score)
+console.log(typeof valInNum)
+console.log(valInNum)
+
+let isLoggedIn= 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
