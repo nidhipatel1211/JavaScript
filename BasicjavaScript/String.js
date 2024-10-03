cd0c44d450c740a4613a5952cd7fbfa66ea5d74e
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2024-10-03 19:46:39 +0000

    String

---
+++ BasicjavaScript/String.js
@@ -0,0 +1,14 @@
+const name="Nidhi"
+const repoc=5
+//console.log(name+repoc+" value")
+//String intropolation
+console.log(`Hello my name is ${name} and my repo count is ${repoc} `)
+const gameName= new String('NidhiP')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+
+console.log(gameName.substring(0,3))
+console.log(gameName.slice(-8,3))
+const gameName1= new String('             NidhiP')
+console.log(gameName1.trim())
